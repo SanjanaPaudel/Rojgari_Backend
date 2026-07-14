8bfd6e2a43de5e6c11943ecd62e8ff7fcd97a803
zephyranth-crypto <dinesh.231614@ncit.edu.np>
2026-07-14 17:01:33 +0545

    Issue fixed in accounts/admin.py

--- accounts/admin.py
+++ accounts/admin.py
@@ -2,8 +2,6 @@ from django.contrib import admin
 
 from .models import CustomerProfile, Skill, User, WorkerProfile
 
-admin.site.register(Skill)
-
 
 @admin.register(Skill)
 class SkillAdmin(admin.ModelAdmin):
