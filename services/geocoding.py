ee9f462f9103cbef732af620cb027acd23926c94
zephyranth-crypto <dinesh.231614@ncit.edu.np>
2026-07-15 15:29:17 +0545

    Booking model created !

--- services/geocoding.py
+++ services/geocoding.py
@@ -14,6 +14,7 @@ def reverse_geocode(latitude, longitude):
         "lat" : latitude,
         "lon" : longitude,
         "format" : "json",
+        "accept-language": "en",
     }
 
     headers = {
