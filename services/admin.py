ec34769d1b779b06e85b8d8e79e7abb076d01f7c
zephyranth-crypto <dinesh.231614@ncit.edu.np>
2026-07-15 20:59:58 +0545

    Added info in service/admin.py for test and successful test !

--- services/admin.py
+++ services/admin.py
@@ -1,1 +1,14 @@
 # Register your models here.
+from django.contrib import admin
+
+from .models import Booking, BookingMedia
+
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ("id", "category", "customer", "worker", "status", "created_at")
+    list_filter = ("status", "category")
+    search_fields = ("customer__user__full_name", "description")
+
+@admin.register(BookingMedia)
+class BookingMediaAdmin(admin.ModelAdmin):
+    list_display = ("id", "booking", "media_type")
