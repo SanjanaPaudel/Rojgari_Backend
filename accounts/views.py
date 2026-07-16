7f95218775962a55f176f21748ff30c25bbefc8d
zephyranth-crypto <dinesh.231614@ncit.edu.np>
2026-07-16 19:29:44 +0545

    Updated ruff error

--- accounts/views.py
+++ accounts/views.py
@@ -5,7 +5,6 @@ from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import RefreshToken
 
-from accounts.permissions import IsWorker
 from accounts.serializers import (
     IdentityDocumentSerializer,
     ResendOTPSerializer,
