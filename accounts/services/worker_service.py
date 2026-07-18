32d19076cab33233142f7d1f7d7ecad0e6eed43a
Sanjana <sanjanapaudel42@gmail.com>
2026-07-18 06:41:49 +0545

    error fixed

--- accounts/services/worker_service.py
+++ accounts/services/worker_service.py
@@ -1,7 +1,7 @@
 from django.db import transaction
 from django.shortcuts import get_object_or_404
-# from django.utils import timezone
 
+# from django.utils import timezone
 from accounts.models import Skill, WorkerProfile
 from services.models import BookingMedia, BookingOffer
 
