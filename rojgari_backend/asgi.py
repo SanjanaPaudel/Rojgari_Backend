7ab25e13bbd2990bf85def3de4c1a7c6cff2fc99
zephyranth-crypto <dinesh.231614@ncit.edu.np>
2026-07-29 15:06:34 +0545

    setup for django web channels

--- rojgari_backend/asgi.py
+++ rojgari_backend/asgi.py
@@ -9,8 +9,22 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/asgi/
 
 import os
 
-from django.core.asgi import get_asgi_application
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "rojgari_backend.settings")
 
-application = get_asgi_application()
+from django.core.asgi import get_asgi_application
+
+django_asgi_app = get_asgi_application()
+
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+
+import rojgari_backend.routing
+
+application = ProtocolTypeRouter(
+    {
+        "http": django_asgi_app,
+        "websocket": AuthMiddlewareStack(
+            URLRouter(rojgari_backend.routing.websocket_urlpatterns)
+        ),
+    }
+)
---
+++ rojgari_backend/routing.py
@@ -0,0 +1,5 @@
+from django.urls import re_path
+
+websocket_urlpatterns = [
+    # WebSocket routes will be added here in Step 7
+]
