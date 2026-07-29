c2928499d25cc931e149a18ebf8a24bf726aa31c
zephyranth-crypto <dinesh.231614@ncit.edu.np>
2026-07-29 21:24:37 +0545

    Added routing records

--- rojgari_backend/routing.py
+++ rojgari_backend/routing.py
@@ -1,3 +1,10 @@
+
+from django.urls import re_path
+
+from accounts.consumers import WorkerOfferConsumer
+from services.consumers import BookingStatusConsumer
+
 websocket_urlpatterns = [
-    # WebSocket routes will be added here in Step 7
+    re_path(r"^ws/worker/offers/$", WorkerOfferConsumer.as_asgi()),
+    re_path(r"^ws/bookings/(?P<booking_id>\d+)/$", BookingStatusConsumer.as_asgi()),
 ]
